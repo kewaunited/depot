7a991f2f4a1f099b2ba6ee0d6b6626688d796967
Kevin-Leong <kevin.leong.cn@gmail.com>
2015-06-16 11:37:09 +0800

    A Smarter Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,11 +27,10 @@ before_action :set_cart, only: [:create]
   # POST /line_items.json
   def create
 product = Product.find(params[:product_id])
-@line_item = @cart.line_items.build(product: product)
+@line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-	format.html { redirect_to @line_item.cart,
-notice: 'Line item was successfully created.' }
+	format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -72,6 +71,6 @@ notice: 'Line item was successfully created.' }
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+params.require(:line_item).permit(:product_id)
     end
 end
