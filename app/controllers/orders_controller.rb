9995b8172e31d256e5bf3b59671eabe511a24f3d
Kevin-Leong <kevin.leong.cn@gmail.com>
2015-06-18 14:57:11 +0800

    Internationalization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,13 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class OrdersController < ApplicationController
-skip_before_action :authorize, only: [:new, :create]
+  skip_before_action :authorize, only: [:new, :create]
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
@@ -18,9 +26,10 @@ skip_before_action :authorize, only: [:new, :create]
   # GET /orders/new
   def new
     if @cart.line_items.empty?
-	redirect_to store_url, notice: "Your cart is empty"
-	return
+      redirect_to store_url, notice: "Your cart is empty"
+      return
     end
+
     @order = Order.new
   end
 
@@ -32,17 +41,20 @@ skip_before_action :authorize, only: [:new, :create]
   # POST /orders.json
   def create
     @order = Order.new(order_params)
-@order.add_line_items_from_cart(@cart)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
-Cart.destroy(session[:cart_id])
-session[:cart_id] = nil
-format.html { redirect_to store_url, notice:'Thank you for your order.' }
-        format.json { render :show, status: :created, location: @order }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_url, notice: 
+          I18n.t('.thanks') }
+        format.json { render action: 'show', status: :created,
+          location: @order }
       else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.html { render action: 'new' }
+        format.json { render json: @order.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -53,9 +65,9 @@ format.html { redirect_to store_url, notice:'Thank you for your order.' }
     respond_to do |format|
       if @order.update(order_params)
         format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
@@ -66,7 +78,7 @@ format.html { redirect_to store_url, notice:'Thank you for your order.' }
   def destroy
     @order.destroy
     respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.html { redirect_to orders_url }
       format.json { head :no_content }
     end
   end
@@ -81,4 +93,5 @@ format.html { redirect_to store_url, notice:'Thank you for your order.' }
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
+  #...
 end
